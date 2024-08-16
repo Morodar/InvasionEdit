03c01008797917ed0191fdb202aecbf797643745
Morodar <10113399+Morodar@users.noreply.github.com>
2024-08-16 23:46:56 +0200

    binds new 3d models

--- src/domain/lev/constants/entityTypeTo3dModel.ts
+++ src/domain/lev/constants/entityTypeTo3dModel.ts
@@ -4,9 +4,17 @@ import { Group, Object3DEventMap } from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 
 const mapping: Map<number, string> = new Map<number, string>([
-    [300, "headquarter.obj"],
-    [380, "high-wall.obj"],
-    [382, "panzersperre.obj"],
+    [300, "neu/Hautgebäude.obj"],
+    [301, "neu/Fabrik.obj"],
+    [310, "neu/Kraftwerk.obj"],
+    [374, "neu/leicheterGeschützturm.obj"],
+    [380, "neu/hoheMauer.obj"],
+    [380, "neu/Mauer.obj"],
+    [382, "neu/Panzersperre.obj"],
+    [330, "neu/Xenitmine.obj"],
+    [331, "neu/Xenitlager.obj"],
+    [332, "neu/Tritiumpumpe.obj"],
+    [333, "neu/Tritiumtank.obj"],
 ]);
 
 export function entityTypeTo3dModel(type: number): string {
