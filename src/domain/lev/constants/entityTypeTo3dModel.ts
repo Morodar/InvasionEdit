7f5219a1f0d1a4c8a3038969a5afffb176d7e015
Morodar <10113399+Morodar@users.noreply.github.com>
2024-08-17 17:25:51 +0200

    extends entityTypeTo3dModel to support glb/gltf/fbx files

--- src/domain/lev/constants/entityTypeTo3dModel.ts
+++ src/domain/lev/constants/entityTypeTo3dModel.ts
@@ -2,6 +2,7 @@ import { useLoader } from "@react-three/fiber";
 import { useMemo } from "react";
 import { Group, Object3DEventMap } from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { GLTFLoader, FBXLoader, GLTF } from "three/examples/jsm/Addons.js";
 
 const mapping: Map<number, string> = new Map<number, string>([
     [300, "headquarter.obj"],
@@ -14,9 +15,37 @@ export function entityTypeTo3dModel(type: number): string {
     return "3d/" + model;
 }
 
+type LoaderResult = Group<Object3DEventMap> | GLTF;
+
+function isGltf(result: LoaderResult): result is GLTF {
+    return (result as GLTF).scene !== undefined;
+}
+
 export function useEntityModel(type: number) {
     const modelUrl = entityTypeTo3dModel(type);
-    const model: Group<Object3DEventMap> = useLoader(OBJLoader, modelUrl);
-    const clone = useMemo(() => model.clone(), [model]);
+    const model: LoaderResult = useLoader(getLoader(modelUrl), modelUrl);
+    const clone = useMemo(() => {
+        if (isGltf(model)) {
+            return model.scene.clone();
+        } else {
+            return model.clone();
+        }
+    }, [model]);
     return clone;
+}
+
+function getLoader(modelUrl: string) {
+    if (modelUrl.endsWith(".obj")) {
+        return OBJLoader;
+    }
+    if (modelUrl.endsWith(".glb")) {
+        return GLTFLoader;
+    }
+    if (modelUrl.endsWith(".gltf")) {
+        return GLTFLoader;
+    }
+    if (modelUrl.endsWith(".fbx")) {
+        return FBXLoader;
+    }
+    return OBJLoader;
 }
