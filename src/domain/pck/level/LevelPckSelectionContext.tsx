508e423b677a128687da342f2d8d4775ad965007
Morodar <10113399+Morodar@users.noreply.github.com>
2024-12-15 13:04:08 +0100

    fixes bug where lev / fld changes are not properly applied to the level pck

--- src/domain/pck/level/LevelPckSelectionContext.tsx
+++ src/domain/pck/level/LevelPckSelectionContext.tsx
@@ -25,47 +25,61 @@ export const LevelPckSelectionContextProvider: React.FC<PropsWithChildren> = ({ 
     const { levelPck, setLevelPck } = useEditLevelContext();
     const { fldFile, dispatch: fldDispatch } = useFldMapContext();
     const { levFile, dispatch: levDispatch } = useLevContext();
-    const [selectedLevel, setSelectedLevel] = useState<Level>();
+    const [selectedLevelIndex, setSelectedLevelIndex] = useState<number>();
+    const selectedLevel = levelPck?.levels.at(selectedLevelIndex ?? 0);
 
     const selectLevel = useCallback(
         (level: Level) => {
-            setSelectedLevel(level);
-
-            // take context state and update levelPck
-            setLevelPck((prevPck: LevelPck | undefined) => {
-                if (!prevPck || !selectedLevel) {
-                    return prevPck;
-                }
-                const updated: LevelPck = { ...prevPck };
-
-                const levelIndex = levelPck?.levels.indexOf(selectedLevel) ?? -1;
-                if (levelIndex === -1 || !levFile || !fldFile) {
-                    return updated;
-                }
-                updated.levels[levelIndex] = {
-                    dat: prevPck.levels[levelIndex].dat,
-                    fld: fldFile,
-                    lev: levFile,
-                };
-
-                return updated;
-            });
-
-            // update context state
+            const levelIndex = levelPck?.levels.indexOf(level);
+            setSelectedLevelIndex(levelIndex);
             fldDispatch({ type: "SET_FLD", fldFile: level.fld });
             levDispatch({ type: "SET_LEV", levFile: level.lev });
         },
-        [fldDispatch, fldFile, levDispatch, levFile, levelPck?.levels, selectedLevel, setLevelPck],
+        [fldDispatch, levDispatch, levelPck?.levels],
     );
 
     useEffect(() => {
-        if (levelPck && !selectedLevel && levelPck.levels.length > 0) {
-            const level = levelPck.levels[0];
-            selectLevel(level);
+        // select first level when levelPck changes
+        if (levelPck && selectedLevelIndex == undefined && levelPck.levels.length > 0) {
+            const firstLevel = levelPck.levels[0];
+            selectLevel(firstLevel);
+            setSelectedLevelIndex(0);
         } else if (!levelPck) {
-            setSelectedLevel(undefined);
+            setSelectedLevelIndex(undefined);
         }
-    }, [fldDispatch, levelPck, selectLevel, selectedLevel]);
+    }, [levelPck, selectLevel, selectedLevelIndex]);
+
+    useEffect(() => {
+        if (selectedLevelIndex == undefined || levFile == undefined) {
+            return;
+        }
+        // sync lev changes
+        setLevelPck((pck) => {
+            if (pck && pck.levels[selectedLevelIndex].lev !== levFile) {
+                const updated: LevelPck = { ...pck };
+                updated.levels[selectedLevelIndex].lev = levFile;
+                return updated;
+            } else {
+                return pck;
+            }
+        });
+    }, [levFile, selectedLevelIndex, setLevelPck]);
+
+    useEffect(() => {
+        if (selectedLevelIndex == undefined || fldFile == undefined) {
+            return;
+        }
+        // sync fld changes
+        setLevelPck((pck) => {
+            if (pck && pck.levels[selectedLevelIndex].fld !== fldFile) {
+                const updated: LevelPck = { ...pck };
+                updated.levels[selectedLevelIndex].fld = fldFile;
+                return updated;
+            } else {
+                return pck;
+            }
+        });
+    }, [fldFile, selectedLevelIndex, setLevelPck]);
 
     const value: LevelPckSelectionContextProps = useMemo(
         () => ({ selectedLevel, selectLevel }),
